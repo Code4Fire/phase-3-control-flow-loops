dac425cd34e8634695c385c0c40c66bba6aea7f1
Tiffany Tam <tamtiffany89@gmail.com>
2021-11-22 11:25:44 -0500

    commit tree 26ceef03f84ba2de086087404fbffc221885cbce

--- looping.rb
+++ looping.rb
@@ -1,6 +1,15 @@
+require pry
+
 def happy_new_year
-  # your code here
+  counter = 10
+  while counter > 0
+    binding.pry
+    puts counter
+    counter --
+
 end
+  puts "Happy New Year!"
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -16,9 +25,37 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+# calls fizzbuzz 100 times from 1 to 100 and printe the ouput]
+
+# 1 strategy using range
+  range = (1. . 100)
+
+  range.each do |num|
+    puts fizzbuzz(num)
+# use while loo
+  counter = 1
+
+  while counter <= 100 
+    puts fizzzbuzz(counter)
+    counter += 1
+  end
+
+
 end
 
 def reverse_string(str)
-  # your code here
+  # break up the string to each letter
+    
+  #start from back  
+    counter = 0
+    reversed_astring = ""
+  while counter < str.length
+  #put it into a new string
+    counter += 1
+    reversed_string += str[str.length - counter]
+  #.push is valid in Ruby but can also be used as "<<" (shovel)
+  end
+  #put the new string back together & return
+    reversed_array.join("")
+
 end
